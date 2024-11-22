4b8867479b40ff80743ac18a97d2820c9a7b62df
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-11-22 11:46:23 +0530

    added validate with helper :zap:

---
+++ validate/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"strings"
+)
+
+func capitalizeFirstLetter(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	return strings.ToUpper(string(s[0])) + s[1:]
+}
+
+func recur(m *map[string]interface{}, key1 string, s *string, validateLogic *string, parent string) {
+	structDef := "type " + capitalizeFirstLetter(key1) + "Type struct {\n"
+	validationLogic := ""
+
+	for key, value := range *m {
+		switch val := value.(type) {
+		case string:
+			var fieldType string
+			var min, max int
+			minProvided, maxProvided := false, false
+
+			parts := strings.Fields(val)
+			fieldType = parts[0]
+
+			for _, part := range parts[1:] {
+				if strings.HasPrefix(part, "min=") {
+					fmt.Sscanf(part, "min=%d", &min)
+					minProvided = true
+				} else if strings.HasPrefix(part, "max=") {
+					fmt.Sscanf(part, "max=%d", &max)
+					maxProvided = true
+				}
+			}
+			structDef += fmt.Sprintf("\t%s %s `json:\"%s\"`\n", capitalizeFirstLetter(key), fieldType, key)
+			fieldPath := parent + capitalizeFirstLetter(key)
+			if fieldType == "string" {
+				if minProvided {
+					validationLogic += fmt.Sprintf("gt(len(%s), %d) && ", fieldPath, min)
+				}
+				if maxProvided {
+					validationLogic += fmt.Sprintf("lt(len(%s), %d) && ", fieldPath, max)
+				}
+			} else {
+				if minProvided {
+					validationLogic += fmt.Sprintf("gt(%s, %d) && ", fieldPath, min)
+				}
+				if maxProvided {
+					validationLogic += fmt.Sprintf("lt(%s, %d) && ", fieldPath, max)
+				}
+			}
+
+		case map[string]interface{}:
+			nestedType := capitalizeFirstLetter(key1 + capitalizeFirstLetter(key))
+			structDef += fmt.Sprintf("\t%s %sType `json:\"%s\"`\n", capitalizeFirstLetter(key), nestedType, key)
+			recur(&val, nestedType, s, validateLogic, parent+capitalizeFirstLetter(key)+".")
+		default:
+			fmt.Println("Error!")
+		}
+	}
+	structDef += "}\n\n"
+	*s = structDef + *s
+	*validateLogic += validationLogic
+}
+
+func main() {
+	Schema := `{
+		"empName": "string min=5 max=50",
+		"id": "int min=1 max=100",
+		"email": "string min=10 max=100",
+		"address": {
+		  "street": "string min=10 max=100",
+		  "city": "string min=3 max=50",
+		  "zip": "int max=9999",
+		  "country": {
+			"name": "string min=3 max=50",
+			"code": "string min=2 max=10"
+		  }
+		},
+		"preferences": {
+		  "theme": "string min=3 max=20",
+		  "notificationsEnabled": "bool",
+		  "language": "string min=2 max=10"
+		}
+	  }`
+	var schema map[string]interface{}
+	json.Unmarshal([]byte(Schema), &schema)
+	code := ""
+	validateLogic := ""
+	recur(&schema, "User", &code, &validateLogic, "u.")
+	validateFunction := "func Validate(u *UserType) bool {\n\treturn " + validateLogic[:len(validateLogic)-4] + "\n}\n\n"
+	code = validateFunction + code
+	fmt.Println(code)
+}

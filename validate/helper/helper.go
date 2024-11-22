6fdd16822e9fc75360cbef36759e70bfcef818a4
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-11-22 12:49:08 +0530

    added  email in rec  with helper :zap:

--- validate/helper/helper.go
+++ validate/helper/helper.go
@@ -1,9 +1,15 @@
 package main
-
+import(
+    "regexp"
+)
 func gt(a, b int) bool {
     return a > b
 }
 
 func lt(a, b int) bool {
     return a < b
+}
+func validateEmail(email string) bool {
+    re := regexp.MustCompile(`^(?i)[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$`)
+    return re.MatchString(email)
 }

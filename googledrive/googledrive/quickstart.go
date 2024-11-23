2e48b10f9874ab84d28e6ad3b15d3064f424295f
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-11-23 22:58:50 +0530

    Added Google drive function :zap:

---
+++ googledrive/googledrive/quickstart.go
@@ -0,0 +1,67 @@
+package googledrive
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "os"
+
+    "google.golang.org/api/drive/v3"
+    "google.golang.org/api/option"
+    // "github.com/joho/godotenv" // Import the godotenv package
+)
+
+func Googledrive(CredentialsPath string, UploadFilePath string ) {
+    // Load the .env file
+    // err := godotenv.Load(".env")
+    // if err != nil {
+    //     log.Fatalf("Error loading .env file: %v", err)
+    // }
+     
+    serviceAccount := CredentialsPath
+    filename := UploadFilePath
+
+    if serviceAccount == "" || filename == "" {
+        log.Fatal("SERVICE_ACCOUNT_JSON or UPLOAD_FILE_PATH not set in .env file")
+    }
+    ctx := context.Background()
+    srv, err := drive.NewService(ctx, option.WithCredentialsFile(serviceAccount), option.WithScopes(drive.DriveScope))
+    if err != nil {
+        log.Fatalf("Unable to create Drive client: %v", err)
+    }
+    file, err := os.Open(filename)
+    if err != nil {
+        log.Fatalf("Unable to open file: %v", err)
+    }
+    defer file.Close()
+    info, err := file.Stat()
+    if err != nil {
+        log.Fatalf("Unable to get file info: %v", err)
+    }
+    f := &drive.File{
+        Name:    info.Name(),
+        MimeType: "text/plain", 
+   
+    }
+
+    uploadedFile, err := srv.Files.Create(f).
+        Media(file).
+        ProgressUpdater(func(now, size int64) { fmt.Printf("Upload progress: %d/%d bytes\r", now, size) }).
+        Do()
+    if err != nil {
+        log.Fatalf("Unable to upload file: %v", err)
+    }
+
+    permission := &drive.Permission{
+        Type: "anyone",
+        Role: "reader",
+    }
+    _, err = srv.Permissions.Create(uploadedFile.Id, permission).Do()
+    if err != nil {
+        log.Fatalf("Failed to set file permissions: %v", err)
+    }
+
+
+    shareableLink := fmt.Sprintf("https://drive.google.com/file/d/%s/view?usp=sharing", uploadedFile.Id)
+    fmt.Printf("File uploaded successfully! Access it here: %s\n", shareableLink)
+}
---
+++ googledrive/main.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	drive "quickstart/googledrive"
+)
+func main(){
+drive.Googledrive("credentials.json","dummy.csv")
+}
